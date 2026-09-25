b6aac8724fb95598b6c2d6bb6017c5ce7580d3aa
gregetarian <78038105+gregetarian@users.noreply.github.com>
2026-09-25 17:06:35 +0100

    Bust categorical legend cache

--- comic/web/app/main.js
+++ comic/web/app/main.js
@@ -18,7 +18,7 @@ import { createPresetsUI, randomColormapName } from '../controls/style-presets.j
 import { contentBBoxPx } from '../core/bbox.js?v=atlas-categorical-v1';
 import { loadBaseScene, buildOverlayMeshes, buildCutVolume, loadOverlayArrays, loadAnatomyVolume, loadParcellation, loadParcellationIndex } from '../scene/asset-loader.js?v=atlas-categorical-v1';
 import { createEngine } from '../scene/renderer.js?v=atlas-categorical-v1';
-import { createColorbar } from '../controls/colorbar.js?v=shared-legend-v1';
+import { createColorbar } from '../controls/colorbar.js?v=atlas-categorical-v1';
 import { initKapow } from '../controls/kapow.js?v=atlas-categorical-v1';
 import { bindGlobalControls, buildOverlayRows } from '../controls/bind.js?v=atlas-categorical-v1';
 import { buildRenderText, usesFigureSpec, buildSpec } from '../controls/cli-export.js?v=lossless-cli-v2';
